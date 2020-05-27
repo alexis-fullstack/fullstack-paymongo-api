08272398c1b0d8f4743cc94ab58514fc8b673f22
Alexis Santos <alxssnts@gmail.com>
2020-05-27 14:59:46 +0800

    3DS Implementation

--- routes/payment.js
+++ routes/payment.js
@@ -1,66 +1,55 @@
 var express = require("express");
 var router = express.Router();
 var Paymongo = require('paymongo');
+const cors = require('cors');
 
 const paymongo = new Paymongo(process.env.SECRET_KEY || "sk_test_amdL4FT9xNotTy5YSV3LsZRS");
 
-router.post("/", function(request, response) {
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+  credentials: true,
+  allowedHeaders: ['Content-Type']
+}
 
-  const expiry = request.body.expiry;
-  const expiryArray = expiry.split('/');
+router.post("/", cors(corsOptions), async function(request, response) {
+  try {
+    const expiry = request.body.expiry;
+    const expiryArray = expiry.split('/');
 
-  const tokenData = { 
-    data: {
-      attributes: {
-        billing: {
-          name: request.body.name,
-          email: request.body.email,
-        },
-        number: request.body.number,
-        exp_month: parseInt(expiryArray[0]),
-        exp_year: parseInt(expiryArray[1]),
-        cvc: request.body.cvc,
+    // const tokenData = { 
+    //   data: {
+    //     attributes: {
+    //       billing: {
+    //         name: request.body.name,
+    //         email: request.body.email,
+    //       },
+    //       number: request.body.number,
+    //       exp_month: parseInt(expiryArray[0]),
+    //       exp_year: parseInt(expiryArray[1]),
+    //       cvc: request.body.cvc,
+    //     }
+    //   }
+    // }
+
+    const payload = {
+      data: {
+        attributes: {
+          amount: request.body.paymentAmount, 
+          currency: 'PHP', 
+          payment_method_allowed: ['card'] 
+        }
       }
     }
-  }
 
-  paymongo
-  .createToken(tokenData)
-  .then(res => {
-    generated_token = res.data.id;
-    const amount = request.body.paymentAmount;
-    const decimal = request.body.decimal;
-    const strAmount = amount.toString() + decimal;
-    const totalAmount = parseInt(strAmount);
-  
-    paymongo
-      .createPayment({
-        data: {
-          attributes: {
-            amount: totalAmount,
-            currency: request.body.currency,
-            description: request.body.description,
-            statement_descriptor: request.body.statement_descriptor,
-            source: {
-              id: generated_token,
-              type: 'token'
-            }
-          } 
-        }
-      })
-      .then(res => {
-        const data = { error: false, res }
-        response.json(data);
-      })
-      .catch(error => {
-        const data = { error : error.message }
-        response.json(data);
-      });
-  })
-  .catch(error => {
-    const data = { error : error.message }
-    response.json(data);
-  });
+    const paymentIntent = await paymongo.paymentIntents.create(payload);
+
+    const { paymentAmount } = request.body;
+
+    response.json(paymentIntent);
+  } catch (err) {
+    response.json(err)
+  }
 });
 
 module.exports = router;
